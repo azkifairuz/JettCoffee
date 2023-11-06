fc5aa8b389660a06a3600037a696fc6b6a9f5883
azkifairuz <azkiajmal@gmail.com>
2023-11-06 19:47:58 +0700

    setup project

---
+++ app/src/main/java/com/azkifairuz/jetcoffee/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.azkifairuz.jetcoffee
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.azkifairuz.jetcoffee.ui.theme.JetCoffeeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetCoffeeTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+
+//home
+@Composable
+fun Home(modifier: Modifier = Modifier){
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomePreview() {
+    JetCoffeeTheme {
+        Home()
+    }
+}
+// end home
+
+@Composable
+fun Categories() {
+
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JetCoffeeTheme {
+        Greeting("Android")
+    }
+}
