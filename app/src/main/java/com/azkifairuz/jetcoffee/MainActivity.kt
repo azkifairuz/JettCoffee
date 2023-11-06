7bc06338f7e4405fc74260c3922868e7262c3d09
azkifairuz <azkiajmal@gmail.com>
2023-11-06 20:58:41 +0700

    create banner

--- app/src/main/java/com/azkifairuz/jetcoffee/MainActivity.kt
+++ app/src/main/java/com/azkifairuz/jetcoffee/MainActivity.kt
@@ -1,15 +1,25 @@
 package com.azkifairuz.jetcoffee
 
+import Search
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.azkifairuz.jetcoffee.ui.theme.JetCoffeeTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,8 +46,10 @@ fun JetCoffeeApp() {
 
 //home
 @Composable
-fun Home(modifier: Modifier = Modifier){
-
+fun Home(modifier: Modifier = Modifier) {
+        Column {
+            Banner()
+        }
 }
 
 @Preview(showBackground = true)
@@ -49,23 +61,25 @@ fun HomePreview() {
 }
 // end home
 
+
+@Composable
+fun Banner(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.banner),
+            contentDescription = "banner",
+            contentScale = ContentScale.Crop,
+            modifier = modifier
+                .height(180.dp)
+        )
+        Search(modifier = modifier.padding(8.dp))
+    }
+}
+
 @Composable
 fun Categories() {
 
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JetCoffeeTheme {
-        Greeting("Android")
-    }
-}
--- app/src/main/java/com/azkifairuz/jetcoffee/ui/components/Search.kt
+++ app/src/main/java/com/azkifairuz/jetcoffee/ui/components/Search.kt
@@ -1,8 +1,7 @@
-import android.graphics.drawable.shapes.Shape
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
@@ -10,6 +9,8 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -17,6 +18,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.azkifairuz.jetcoffee.ui.theme.JetCoffeeTheme
@@ -25,14 +27,23 @@ import com.azkifairuz.jetcoffee.ui.theme.JetCoffeeTheme
 @Composable
 fun Search(modifier: Modifier = Modifier) {
     var text by remember { mutableStateOf("") }
+    Box(
+        modifier = Modifier.padding(26.dp)
+    ) {
+        TextField(
+            value = text,
+            onValueChange = { text = it },
+            label = { Text("Search") },
+            leadingIcon = { Icon(Icons.Filled.Search, contentDescription = null) },
+            colors = TextFieldDefaults.textFieldColors(containerColor = Color.White),
+            modifier = Modifier
+                .fillMaxWidth()
+                .heightIn(44.dp)
+                .clip(RoundedCornerShape(20.dp))
 
-    TextField(
-        value = text,
-        onValueChange = { text = it },
-        label = { Text("Search") },
-        leadingIcon = { Icon(Icons.Filled.Search, contentDescription = null) },
-        modifier = Modifier.fillMaxWidth().clip(RoundedCornerShape(14.dp))
-    )
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
