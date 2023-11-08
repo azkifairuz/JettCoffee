a8cd3fc8cce7c420ac776eab7f1789eb48e3b16a
azkifairuz <azkiajmal@gmail.com>
2023-11-08 20:17:15 +0700

    create component category

--- app/src/main/java/com/azkifairuz/jetcoffee/MainActivity.kt
+++ app/src/main/java/com/azkifairuz/jetcoffee/MainActivity.kt
@@ -5,12 +5,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -18,8 +22,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.azkifairuz.jetcoffee.ui.components.CategoryItem
 import com.azkifairuz.jetcoffee.ui.theme.JetCoffeeTheme
 
 class MainActivity : ComponentActivity() {
@@ -49,6 +56,8 @@ fun JetCoffeeApp() {
 fun Home(modifier: Modifier = Modifier) {
         Column {
             Banner()
+            SectionText(title = stringResource(R.string.section_category))
+            CategoryRow()
         }
 }
 
@@ -79,7 +88,34 @@ fun Banner(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun Categories() {
+fun SectionText(
+    title : String,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = title,
+        style = MaterialTheme.typography.headlineSmall.copy(
+            fontWeight = FontWeight.ExtraBold
+        ),
+        modifier = modifier
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+    )
+    
+}
 
+@Composable
+fun CategoryRow(
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier.padding(vertical = 16.dp)
+    ){
+        items(dummyCategory,key = {it.textCategory}){ category ->
+            CategoryItem(categories = category)
+        }
+    }
+    
 }
 
---
+++ app/src/main/java/com/azkifairuz/jetcoffee/ui/components/CategoryItem.kt
@@ -0,0 +1,63 @@
+package com.azkifairuz.jetcoffee.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.azkifairuz.jetcoffee.Categories
+import com.azkifairuz.jetcoffee.R
+import com.azkifairuz.jetcoffee.ui.theme.JetCoffeeTheme
+
+@Composable
+fun CategoryItem(
+    categories: Categories,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = categories.imageCategory),
+            contentDescription = null,
+            modifier = Modifier
+                .size(60.dp)
+                .clip(CircleShape)
+        )
+        Text(
+            text = stringResource(id = categories.textCategory),
+            fontSize = 10.sp,
+            modifier = Modifier.paddingFromBaseline(
+                top = 16.dp,
+                bottom = 8.dp
+            )
+        )
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CategoryItemPrev() {
+    JetCoffeeTheme {
+        CategoryItem(
+            Categories(
+                R.drawable.icon_category_cappuccino,
+                R.string.category_cappuccino,
+            ),
+            modifier = Modifier.padding(horizontal = 8.dp)
+        )
+    }
+}
